3731f48f5157998e42b7f0a4381386df3e6f2d5c
Saulo Jorenti <saulojorenti@hotmail.com>
2024-10-27 19:12:53 -0300

    Fan Functionality

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 long currentMillis = 0;
 long previousMillis = 0;
+long millisTemp = 0;
 int interval = 1000;
 
 float calibrationFactor = 4.5;
@@ -18,6 +19,7 @@ byte pulse1Sec = 0;
 float flowRate;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
+int currentPwmValue = 0;
 
 MAX6675 thermocouple(CLK_temp, CS_temp, SO_temp);
 
@@ -26,66 +28,130 @@ void IRAM_ATTR pulseCounter()
   pulseCount++;
 }
 
+int getFlowRate(){
+  return int(flowRate);
+}
+
+int getTotalMilliLitres(){
+  return totalMilliLitres;
+}
+
+int getTemperature(){
+  return thermocouple.readCelsius();
+}
+
+void calcFlowRate(){
+
+  currentMillis = millis();
+  if (currentMillis - previousMillis > interval) {
+    pulse1Sec = pulseCount;
+    pulseCount = 0;
+
+    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+    previousMillis = millis();
+
+
+    flowMilliLitres = (flowRate / 60) * 1000;
+
+    totalMilliLitres += flowMilliLitres;
+  }
+}
+
+void setFanSpeed(int speed){
+  currentPwmValue = map(speed, 0, 100, 0, 255);
+  ledcWrite(0,currentPwmValue);
+}
+
+void setResistance(int resistance){
+  digitalWrite(pinage_res,resistance == 0 ? LOW : HIGH);
+}
+
+void printFlowRate(){
+  Serial.print("Vazão: ");
+  Serial.print(flowRate);
+  Serial.print(" L/min\t");
+  Serial.print("Total: ");
+  Serial.print(totalMilliLitres);
+  Serial.println("mL");
+}
+
+void decisionFan(){
+  int temperature = getTemperature();
+  millisTemp = millis();
+
+  if ((millisTemp / 1000) < 10) {
+    setFanSpeed(100);
+    return;
+  }
+
+  if (temperature >= 80 && temperature < 90){
+    setFanSpeed(100);
+  }
+  else if (temperature >= 70 && temperature < 80 ){
+    setFanSpeed(80);
+  }
+  else if (temperature >= 60 && temperature < 70){
+    setFanSpeed(60);
+  }
+  else if (temperature >= 50 && temperature < 60){
+    setFanSpeed(40);
+  }
+  else if (temperature >= 40 && temperature < 50){
+    setFanSpeed(20);
+  }
+
+  else if (temperature >= 90){
+    setResistance(0);
+    Serial.println("Planta Desligada Por Segurança");
+
+  }
+  else{
+    setFanSpeed(0);
+  }
+  
+  
+}
+void printSetup(){
+  int resistaneStatus = digitalRead(pinage_res);
+  Serial.println("Setup Inicializado");
+  Serial.print("Resistência: ");
+  Serial.println(resistaneStatus == 0 ? "Desligada" : "Ligada");
+  Serial.print("Fan Speed Initial: ");
+  Serial.println(map(currentPwmValue, 0, 255, 0, 100) + "%");
+  Serial.println("Flow Sensor Initializado");
+  Serial.println("Thermocouple Initializado");
+  Serial.println("Setup Finalizado");
+}
+
+
 void setup() {
   Serial.begin(115200);
   ledcSetup(0,25000,8);
   ledcAttachPin(pinage_fan,0);
   pinMode(pinage_res,OUTPUT);
-  digitalWrite(pinage_res,HIGH);
-  
   pinMode(pinage_flow_sensor, INPUT_PULLUP);
-
   pulseCount = 0;
   flowRate = 0.0;
   flowMilliLitres = 0;
   totalMilliLitres = 0;
   previousMillis = 0;
-
   attachInterrupt(digitalPinToInterrupt(pinage_flow_sensor), pulseCounter, FALLING);
+  setFanSpeed(100);
+  setResistance(0);
+  printSetup();
 }
 
 void loop() {
-  ledcWrite(0,255); 
-  delay(1000);
-    //temp
-  Serial.print("C = "); 
-  Serial.println(thermocouple.readCelsius());
-  Serial.print("F = ");
-  Serial.println(thermocouple.readFahrenheit());
-currentMillis = millis();
-  if (currentMillis - previousMillis > interval) {
-    
-    pulse1Sec = pulseCount;
-    pulseCount = 0;
-
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
-    previousMillis = millis();
-
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-
-    // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
-    
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print("L/min");
-    Serial.print("\t");       // Print tab space
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("Output Liquid Quantity: ");
-    Serial.print(totalMilliLitres);
-    Serial.print("mL / ");
-    Serial.print(totalMilliLitres / 1000);
-    Serial.println("L");
-  }
-  
+  setFanSpeed(100);
+  setResistance(0);// LIGA A RESISTÊNCIA
+  calcFlowRate();
+  decisionFan();
+  printFlowRate();
 }
+
+
+
+
+
+
+
